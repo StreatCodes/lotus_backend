9fc73b378e34ac4ed99ae6cef468ccf4fbffd362
StreatCodes <matt.r.streatfield@gmail.com>
2018-08-02 22:16:20 +1000

    Additional tables and fields

--- tables.sql
+++ tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE pages (
-	id		INTEGER PRIMARY KEY,
-	title	TEXT,
-	slug	TEXT
+	id			INTEGER PRIMARY KEY,
+	title		TEXT,
+	slug		TEXT
 );
 
 CREATE TABLE users (
@@ -12,8 +12,15 @@ CREATE TABLE users (
 );
 
 CREATE TABLE sessions (
-	user_id INTEGER REFERENCES users,
-	key TEXT,
-	created_ip TEXT
+	user_id 	INTEGER REFERENCES users,
+	key 		TEXT,
+	created_ip 	TEXT
 );
 CREATE INDEX session_index ON sessions(user_id);
+
+CREATE TABLE components (
+	id			INTEGER PRIMARY KEY,
+	page_id 	INTEGER REFERENCES pages,
+	sort		INTEGER,
+	data		STRING
+);
