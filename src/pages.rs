1a51cd15881d69817e6ffdb09f9913d1f52adba7
StreatCodes <matt.r.streatfield@gmail.com>
2018-08-25 16:02:10 +1000

    Initial golang setup

--- src/pages.rs
+++
@@ -1,9 +0,0 @@
-use warp::Filter;
-use warp::filters::BoxedFilter;
-use warp::{path, header};
-
-pub fn bye_handler() -> BoxedFilter<(i32, String)> {
-    path::param::<i32>()
-        .and(header::<String>("host"))
-        .boxed()
-}
